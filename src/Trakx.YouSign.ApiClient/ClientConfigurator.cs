be31b47beccdcd3837819e9538715fe61137e05a
rafael@trakx.io <rafael@trakx.io>
2021-07-12 18:00:03 -0300

    Added initial version of the files.

---
+++ src/Trakx.YouSign.ApiClient/ClientConfigurator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Trakx.YouSign.ApiClient.Utils;
+using Trakx.Utils.Apis;
+
+namespace Trakx.YouSign.ApiClient
+{
+    internal class ClientConfigurator
+    {
+        private readonly IServiceProvider _provider;
+
+        public ClientConfigurator(IServiceProvider provider)
+        {
+            _provider = provider;
+            Configuration = provider.GetService<IOptions<YouSignApiConfiguration>>()!.Value;
+        }
+
+        public YouSignApiConfiguration Configuration { get; }
+
+        public ICredentialsProvider GetCredentialsProvider(System.Type clientType)
+        {
+            return _provider.GetRequiredService<ICredentialsProvider>();
+        }
+    }
+}
---
+++ src/Trakx.YouSign.ApiClient/YouSignApiConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trakx.Utils.Attributes;
+
+namespace Trakx.YouSign.ApiClient
+{
+    public record YouSignApiConfiguration
+    {
+#nullable disable
+        [Required]
+        public string BaseUrl { get; init; }
+
+        public int? InitialRetryDelayInMilliseconds { get; init; }
+
+        public int? MaxRetryCount { get; init; }
+
+        [SecretEnvironmentVariable]
+        public string ApiKey { get; init; }
+
+#nullable restore
+    }
+}
